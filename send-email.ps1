226daecbf05f547c192fc3e2e9cbb214bdfa8437
sudarsana-reddy <sudarsana.ala@gmail.com>
2022-07-26 12:02:08 +0530

    Update send-email.ps1

--- send-email.ps1
+++ send-email.ps1
@@ -10,28 +10,7 @@ param (
 
    [Parameter()]
    [System.String[]]
-   $toEmail,
-
-   [Parameter()]
-   [System.String]
-   $buildURL,
-
-   [Parameter()]
-   [System.String]
-   $appName,
-
-   [Parameter()]
-   [System.String]
-   $environment,
-
-   [Parameter()]
-   [System.String]
-   $module,
-
-   [Parameter()]
-   [System.String]
-   $emailReport
-
+   $toEmail  
 
 )
 
@@ -45,17 +24,11 @@ try{
       write-host $email;
       $message.To.Add($email);
    }
-   $buildDetails = $buildURL.Split("=");
-   $buildId = $buildDetails[$buildDetails.Length -1]
-   write-host $buildId;
-   $message.Subject = (Get-Culture).TextInfo.ToTitleCase("Build#${buildId}: $appName-$module-$environment-Results");
-   $message.isbodyhtml = $true;   
-   $htmlText = Get-Content $emailReport -Raw;
-   write-host $buildURL; 
-   $message.Body = $htmlText.replace("{{BUILD_URL}}",$buildURL);
   
+   $message.Subject = "GitHub Notificaction"
+   $message.Body = "Update the status and other details"  
    $credentials = New-Object System.Net.NetworkCredential($Username, $Password);
-   $smtp = new-object Net.Mail.SmtpClient("smtp.office365.com", "587");
+   $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", "587");
    $smtp.EnableSSL = $true;
    $smtp.Credentials = $credentials;
    $smtp.send($message);
